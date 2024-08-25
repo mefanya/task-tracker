598416682a1b5433d84d09d8bce0b0eb963a2110
mefanya <ms1604@bk.ru>
2024-08-25 16:19:40 +0300

    Выполнение Домашки

---
+++ pythonProject/app.py
@@ -0,0 +1,38 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    filename = "index.html"
+
+    def get_context_data(self):
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            context = file.read()
+
+        return context
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(self.get_context_data(), "utf-8"))  # Тело ответа
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
